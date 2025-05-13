1824d51e3daed9c517183835c708118e68bcd752
rianadb <rianadbejarin@gmail.com>
2025-05-13 20:41:05 +0800

    feat: add cards for jobs in find job page

--- src/findjob/views.py
+++ src/findjob/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 def find_jobs_view(request, *args, **kwargs):
-    return render(request, "jobs.html", {})
+    if request.GET.get("job"):
+        ...
+
+    context = {
+        # "form" : form,
+        # "jobs" : jobs,
+    }
+    return render(request, "jobs.html", context)
